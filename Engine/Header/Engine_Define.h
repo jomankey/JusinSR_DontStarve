fe9fbabf8b387c30aa54bd798107507e67d28c10
jomankey <joidu03@gmail.com>
2024-01-24 23:08:38 +0900

    UPdate
    dd

--- Engine/Header/Engine_Define.h
+++ Engine/Header/Engine_Define.h
@@ -24,19 +24,21 @@
 
 #pragma warning(disable : 4251)
 
-//#ifdef _DEBUG
-//
-//#define _CRTDBG_MAP_ALLOC
-//#include <stdlib.h>
-//#include <crtdbg.h>
-//
-//#ifndef DBG_NEW 
-//
-//#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ ) 
-//#define new DBG_NEW 
-//
-//#endif
-//#endif
+#if defined(_DEBUG) && !defined(_TOOL)
+
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+#ifndef DBG_NEW 
+
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ ) 
+#define new DBG_NEW 
+
+
+#endif 
+
+#endif
 
 using namespace std;
 using namespace Engine;
