3eec0f17778f4f524523645bfcfcc0d3a13eeac4
jomankey <joidu03@gmail.com>
2024-01-25 17:55:00 +0900

    Revert "Revert "UI추가 카메라연동 완료""
    This reverts commit 7714277619dae4536e56100346cc3d630125c7b2.

--- Client/Code/Stage.cpp
+++ Client/Code/Stage.cpp
@@ -9,6 +9,7 @@
 #include "DynamicCamera.h"
 #include "SkyBox.h"
 #include "Effect.h"
+#include "CUI.h"
 
 CStage::CStage(LPDIRECT3DDEVICE9 pGraphicDev)
 	: Engine::CScene(pGraphicDev)
@@ -25,6 +26,7 @@ HRESULT CStage::Ready_Scene()
 	FAILED_CHECK_RETURN(Ready_Layer_Environment(L"Environment"), E_FAIL);
 	FAILED_CHECK_RETURN(Ready_Layer_GameLogic(L"GameLogic"), E_FAIL);
 	FAILED_CHECK_RETURN(Ready_Layer_UI(L"UI"), E_FAIL);
+	FAILED_CHECK_RETURN(Ready_Layer_GameRes(L"GameRes"), E_FAIL);
 	FAILED_CHECK_RETURN(Ready_LightInfo(), E_FAIL);
 
 
@@ -112,10 +114,23 @@ HRESULT CStage::Ready_Layer_UI(const _tchar* pLayerTag)
 {
 	Engine::CLayer* pLayer = Engine::CLayer::Create();
 	NULL_CHECK_RETURN(pLayer, E_FAIL);
-
 	Engine::CGameObject* pGameObject = nullptr;
 
+	pGameObject = CUI::Create(m_pGraphicDev,CUI::UI_STATE::UI_STATIC,_vec3(0.f,0.f,0.f),_vec3(100.f,100.f,0.f));
+	NULL_CHECK_RETURN(pGameObject, E_FAIL);
+	FAILED_CHECK_RETURN(pLayer->Add_GameObject(L"UI", pGameObject), E_FAIL);
 
+	m_mapLayer.insert({ pLayerTag, pLayer });
+
+	return S_OK;
+}
+
+HRESULT CStage::Ready_Layer_GameRes(const _tchar* pLayerTag)
+{
+	Engine::CLayer* pLayer = Engine::CLayer::Create();
+	NULL_CHECK_RETURN(pLayer, E_FAIL);
+
+	Engine::CGameObject* pGameObject = nullptr;
 
 	m_mapLayer.insert({ pLayerTag, pLayer });
 
