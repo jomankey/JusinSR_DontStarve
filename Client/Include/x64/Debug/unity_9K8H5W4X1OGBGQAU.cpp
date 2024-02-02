79cd193a922ab474ca4603a7749ddea825e3d353
jomankey <joidu03@gmail.com>
2024-02-02 10:02:05 +0900

    UpdateObjectManager
    UpdateObjectManager

---
+++ Client/Include/x64/Debug/unity_9K8H5W4X1OGBGQAU.cpp
@@ -0,0 +1,79 @@
+#include "stdafx.h"
+
+#include "C:\GitHub\JusinSR\Client\Code\Beefalo.cpp"
+
+
+#include "C:\GitHub\JusinSR\Client\Code\CInven.cpp"
+
+
+#include "C:\GitHub\JusinSR\Client\Code\CInvenBox.cpp"
+
+
+#include "C:\GitHub\JusinSR\Client\Code\CInventoryMgr.cpp"
+
+
+#include "C:\GitHub\JusinSR\Client\Code\CItem.cpp"
+
+
+#include "C:\GitHub\JusinSR\Client\Code\CItemFood.cpp"
+
+
+#include "C:\GitHub\JusinSR\Client\Code\CObjectGrass.cpp"
+
+
+#include "C:\GitHub\JusinSR\Client\Code\CObjectRock.cpp"
+
+
+#include "C:\GitHub\JusinSR\Client\Code\CObjectTree.cpp"
+
+
+#include "C:\GitHub\JusinSR\Client\Code\CPigHouse.cpp"
+
+
+#include "C:\GitHub\JusinSR\Client\Code\CUI.cpp"
+
+
+#include "C:\GitHub\JusinSR\Client\Code\DeerClops.cpp"
+
+
+#include "C:\GitHub\JusinSR\Client\Code\DynamicCamera.cpp"
+
+
+#include "C:\GitHub\JusinSR\Client\Code\Effect.cpp"
+
+
+#include "C:\GitHub\JusinSR\Client\Code\Loading.cpp"
+
+
+#include "C:\GitHub\JusinSR\Client\Code\Logo.cpp"
+
+
+#include "C:\GitHub\JusinSR\Client\Code\MainApp.cpp"
+
+
+#include "C:\GitHub\JusinSR\Client\Code\Monster.cpp"
+
+
+#include "C:\GitHub\JusinSR\Client\Code\Pig.cpp"
+
+
+#include "C:\GitHub\JusinSR\Client\Code\Player.cpp"
+
+
+#include "C:\GitHub\JusinSR\Client\Code\SkyBox.cpp"
+
+
+#include "C:\GitHub\JusinSR\Client\Code\Spider.cpp"
+
+
+#include "C:\GitHub\JusinSR\Client\Code\Stage.cpp"
+
+
+#include "C:\GitHub\JusinSR\Client\Code\Terrain.cpp"
+
+
+#include "C:\GitHub\JusinSR\Client\Include\Client.cpp"
+
+
+#include "C:\GitHub\JusinSR\Client\Include\FuncEvent.cpp"
+
---
+++ Engine/System/Include/x64/Debug/unity_C40LYMILMNCNIKSU.cpp
@@ -0,0 +1,36 @@
+
+#include "C:\GitHub\JusinSR\Engine\System\Code\CollisionMgr.cpp"
+
+
+#include "C:\GitHub\JusinSR\Engine\System\Code\CResMgr.cpp"
+
+
+#include "C:\GitHub\JusinSR\Engine\System\Code\CSoundMgr.cpp"
+
+
+#include "C:\GitHub\JusinSR\Engine\System\Code\FontMgr.cpp"
+
+
+#include "C:\GitHub\JusinSR\Engine\System\Code\Frame.cpp"
+
+
+#include "C:\GitHub\JusinSR\Engine\System\Code\FrameMgr.cpp"
+
+
+#include "C:\GitHub\JusinSR\Engine\System\Code\GraphicDev.cpp"
+
+
+#include "C:\GitHub\JusinSR\Engine\System\Code\InputDev.cpp"
+
+
+#include "C:\GitHub\JusinSR\Engine\System\Code\MyFont.cpp"
+
+
+#include "C:\GitHub\JusinSR\Engine\System\Code\CPathMgr.cpp"
+
+
+#include "C:\GitHub\JusinSR\Engine\System\Code\Timer.cpp"
+
+
+#include "C:\GitHub\JusinSR\Engine\System\Code\TimerMgr.cpp"
+
