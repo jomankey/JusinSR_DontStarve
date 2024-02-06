f746783e3fe1cc3c2ab22b4246f3cd099c5dd873
ParkSewon <Jusin142@ruu.kr>
2024-02-06 23:08:00 +0900

    PSW/Obj_Update /Add_FireFlies

--- Client/Code/Stage.cpp
+++ Client/Code/Stage.cpp
@@ -17,6 +17,7 @@
 #include "CPigHouse.h"
 #include "Pig.h"
 #include "BerryBush.h"
+#include"CObjectFireFlies.h"
 
 #include "Transform.h"
 //Scene
@@ -179,12 +180,22 @@ HRESULT CStage::Ready_Layer_GameLogic()
 	//	NULL_CHECK_RETURN(pGameObject, E_FAIL);
 	//	FAILED_CHECK_RETURN(m_vecLayer[(int)eLAYER_TYPE::OBJECTS]->AddGameObject(L"OBJ_ROCK", pGameObject), E_FAIL);
 	//}
-	/*for (_int i = 0; i < 10; ++i)
+	//for (_int i = 0; i < 10; ++i)
+	//{
+	//	pGameObject = CObjectTree::Create(m_pGraphicDev);
+	//	NULL_CHECK_RETURN(pGameObject, E_FAIL);
+	//	FAILED_CHECK_RETURN(m_arrLayer[(int)eLAYER_TYPE::GAME_LOGIC]->AddGameObject(eOBJECT_GROUPTYPE::RESOURCE_OBJECT, pGameObject), E_FAIL);
+	//}
+
+	for (_int i = 0; i < 10; ++i)
 	{
-		pGameObject = CObjectTree::Create(m_pGraphicDev);
+		pGameObject = CObjectFireFlies::Create(m_pGraphicDev);
+		pGameObject->GetTransForm()->Set_Pos(64.f,1,64.f);
 		NULL_CHECK_RETURN(pGameObject, E_FAIL);
 		FAILED_CHECK_RETURN(m_arrLayer[(int)eLAYER_TYPE::GAME_LOGIC]->AddGameObject(eOBJECT_GROUPTYPE::RESOURCE_OBJECT, pGameObject), E_FAIL);
-	}*/
+	}
+
+
 	//pGameObject = CObjectTree::Create(m_pGraphicDev);
 	//NULL_CHECK_RETURN(pGameObject, E_FAIL);
 	//FAILED_CHECK_RETURN(m_arrLayer[(int)eLAYER_TYPE::GAME_LOGIC]->AddGameObject(eOBJECT_GROUPTYPE::RESOURCE_OBJECT, pGameObject), E_FAIL);
@@ -214,6 +225,8 @@ HRESULT CStage::Ready_Layer_GameLogic()
 	//	NULL_CHECK_RETURN(pGameObject, E_FAIL);
 	//	FAILED_CHECK_RETURN(m_arrLayer[(int)eLAYER_TYPE::GAME_LOGIC]->AddGameObject(eOBJECT_GROUPTYPE::RESOURCE_OBJECT, pGameObject), E_FAIL);
 	//}
+
+	
 	//for (_int i = 0; i < 2; ++i)
 	//{
 	//	pGameObject = CItem::Create(m_pGraphicDev, L"Cooked_berries", _vec3(_float(rand() % 20), 1.5f, _float(rand() % 20)));
